0a2b04a7b0ae042da1ee383be481cb7872b6df29
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-09-14 01:41:21 -0700

    Set Up First Page

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import logo from '../assets/Seshspots Logo.png'
+function Header() {
+
+  return (
+    <div className="flex justify-between bg-slate-600 bg-opacity-5 p-5">
+        <img className='' src={logo} />
+        <h1 className='inline-block align-middle text-3xl font-bold'>SeshStops</h1>
+    </div>
+  )
+}
+
+export default Header
